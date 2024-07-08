d44f160888e0c1019796f4a87bf967386715abfc
Dariusz Okoński <darek200180@gmail.com>
2024-07-08 21:13:51 +0200

    return token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -40,7 +42,25 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      return res.json({ user });
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          return res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
