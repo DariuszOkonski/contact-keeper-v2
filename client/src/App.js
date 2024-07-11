dfee7361f6d34278cb9ae035ece730b3e295767d
Dariusz Okoński <darek200180@gmail.com>
2024-07-11 20:29:45 +0200

    working on react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
-  return <div className='App'>My App</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      My App
+    </div>
+  );
 };
 
 export default App;
