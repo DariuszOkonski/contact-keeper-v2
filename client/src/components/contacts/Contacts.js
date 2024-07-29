2dc94489f4cac0e9a07b273083e1d405b9b14103
Dariusz Okoński <darek200180@gmail.com>
2024-07-29 21:37:35 +0200

    add Contacts

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+  const { contacts } = contactContext;
+
+  return (
+    <React.Fragment>
+      {contacts.map((contact) => (
+        <h3>{contact.name}</h3>
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default Contacts;
