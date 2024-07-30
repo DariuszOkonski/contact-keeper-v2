c5e49e8bee30b306fa847d2a64e9fbd47f2336e5
Dariusz Okoński <darek200180@gmail.com>
2024-07-30 15:52:02 +0200

    add form and functionality to add new person

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import {
   ADD_CONTACT,
@@ -42,6 +42,10 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // add contact
+  const addContact = (contact) => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   // delete contact
   // set current contact
   // clear current contact
@@ -50,7 +54,7 @@ const ContactState = (props) => {
   // clear filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
       {props.children}
     </ContactContext.Provider>
   );
